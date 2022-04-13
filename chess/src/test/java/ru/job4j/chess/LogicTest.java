6f327ed76a028e196bd2101eaf709062200a1c8a
pavelberzkaln <p.berzkaln@gmail.com>
2022-04-13 22:35:15 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,18 +1,41 @@
 package ru.job4j.chess;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Ignore
     @Test
-    public void move()
+    public void moveIsSuccessful()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test (expected =  ImpossibleMoveException.class)
+    public void moveGotImpossibleMoveException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C1, Cell.F6);
+    }
+
+    @Test (expected =  OccupiedCellException.class)
+    public void moveGotOccupiedCellException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.H6));
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test (expected =  FigureNotFoundException.class)
+    public void moveGotFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C3, Cell.H6);
     }
 }
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.chess.firuges.black;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+
+public class BishopBlackTest {
+
+    @Test
+    public void testPositionIsSuccessful() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        assertThat(bishopBlack.position(), is(Cell.findBy(2,0)));
+    }
+
+    @Test
+    public void testCopyIsSuccessful() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        assertThat(bishopBlack.copy(Cell.F8).position(), is(Cell.findBy(5,0)));
+    }
+
+    @Test (expected =  ImpossibleMoveException.class)
+    public void wayGotImpossibleMoveException() throws ImpossibleMoveException {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.way(Cell.C5);
+    }
+
+    @Test
+    public void wayIsSuccessful() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] cell = new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Cell[] rsl = bishopBlack.way(Cell.G5);
+        assertThat(cell, is(rsl));
+    }
+}
